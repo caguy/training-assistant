b8c6be0e0ccaf9780b4da203f754d37cd6466a21
Camille Guy <58878482+caguy@users.noreply.github.com>
2025-11-24 07:09:49 +0100

    Remove base path from Vite configuration
    Removed base configuration for the Vite project.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/training-assistant",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
